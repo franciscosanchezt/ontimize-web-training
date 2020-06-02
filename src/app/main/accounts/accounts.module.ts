602677be59a5480b5870876de74519ec0f363c4b
Francisco Sanchez <frango9000@gmail.com>
2020-06-02 18:54:03 +0200

    app-account-number-render column not showing

--- src/app/main/accounts/accounts.module.ts
+++ src/app/main/accounts/accounts.module.ts
@@ -4,15 +4,19 @@ import {CommonModule} from '@angular/common';
 import {AccountsRoutingModule} from './accounts-routing.module';
 import {OntimizeWebModule} from "ontimize-web-ngx";
 import {AccountsHomeComponent} from "./accounts-home/accounts-home.component";
+import {AccountNumberRenderComponent} from "./accounts-home/account-number-render/account-number-render.component";
+import {SharedModule} from "../../shared/shared.module";
 
 @NgModule({
     imports: [
         CommonModule,
         OntimizeWebModule,
+        SharedModule,
         AccountsRoutingModule
     ],
     declarations: [
         AccountsHomeComponent
+
     ]
 })
 export class AccountsModule {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import { OntimizeWebModule } from 'ontimize-web-ngx';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {OntimizeWebModule} from 'ontimize-web-ngx';
+import {CommonModule} from '@angular/common';
+import {AccountNumberRenderComponent} from "../main/accounts/accounts-home/account-number-render/account-number-render.component";
 
 @NgModule({
-  imports: [
-    OntimizeWebModule
-  ],
-  declarations: [
-  ],
-  exports: [
-    CommonModule
-  ]
+    imports: [
+        OntimizeWebModule
+    ],
+    declarations: [
+        AccountNumberRenderComponent
+    ],
+    exports: [
+        CommonModule,
+        AccountNumberRenderComponent
+    ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
