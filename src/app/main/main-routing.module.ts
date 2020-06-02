cdf29887e1d9451074c801fcff9e93395b4df337
Francisco Sanchez <frango9000@gmail.com>
2020-06-02 18:07:23 +0200

    Crear el módulo y formularios de empleados

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,40 +1,41 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AuthGuardService } from 'ontimize-web-ngx';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthGuardService} from 'ontimize-web-ngx';
 
-import { MainComponent } from './main.component';
-import { HomeModule } from './home/home.module';
-import { CustomersModule } from './customers/customers.module';
+import {MainComponent} from './main.component';
+import {HomeModule} from './home/home.module';
+import {CustomersModule} from './customers/customers.module';
+import {EmployeesModule} from "./employees/employees.module";
 
 export function loadHomeModule() {
-  return HomeModule;
+    return HomeModule;
 }
 
 export function loadCustomersModule() {
-  return CustomersModule;
+    return CustomersModule;
+}
+
+export function loadEmployeesModule() {
+    return EmployeesModule;
 }
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: MainComponent,
-    canActivate: [AuthGuardService],
-    children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      {
-        path: 'home',
-        loadChildren: loadHomeModule
-      },
-      {
-        path: 'customers',
-        loadChildren: loadCustomersModule
-      }
-    ]
-  }
+    {
+        path: '',
+        component: MainComponent,
+        canActivate: [AuthGuardService],
+        children: [
+            {path: '', redirectTo: 'home', pathMatch: 'full'},
+            {path: 'home', loadChildren: loadHomeModule},
+            {path: 'customers', loadChildren: loadCustomersModule},
+            {path: 'employees', loadChildren: loadEmployeesModule}
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class MainRoutingModule { }
+export class MainRoutingModule {
+}
